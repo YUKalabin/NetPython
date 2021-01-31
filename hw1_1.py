8129e4eeb2c1b154a17dde79da39f82f31de1792
YUKalabin <74131858+YUKalabin@users.noreply.github.com>
2021-01-31 17:10:14 +0300

    HW1

---
+++ hw1_1.py
@@ -0,0 +1,34 @@
+
+
+# # Задание 1
+# den = input('Input date: ')
+# task = input('Input task: ')
+
+# print(den + ' you must ' + task)
+
+# # Задание 2
+# den0 = input('Input date: ')
+# task0 = input('Input task: ')
+# den1 = input('Input date: ')
+# task1 = input('Input task: ')
+# den2 = input('Input date: ')
+# task2 = input('Input task: ')
+
+# print(den0 + ' you must ' + task0)
+# print(den1 + ' you must ' + task1)
+# print(den2 + ' you must ' + task2)
+
+# Задание 3
+den0 = input('Input date: ')
+task0 = input('Input task: ')
+den1 = input('Input date: ')
+task1 = input('Input task: ')
+den2 = input('Input date: ')
+task2 = input('Input task: ')
+
+alltasks = {
+  den0: task0,
+  den1: task1,
+  den2: task2 
+}
+print(alltasks)
